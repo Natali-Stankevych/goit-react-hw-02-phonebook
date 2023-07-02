fb48edc87f55822c58c2b4c130365c6181043312
Natali-Stankevych <rusakovanatali94@gmail.com>
2023-07-02 15:40:41 +0300

    add

--- src/components/ConactList/Contact.list.styled.jsx
+++ src/components/ConactList/Contact.list.styled.jsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const List = styled.ul`
     display: flex;
     justify-content: center;
-    flex-direction: column;
     width: 400px;
     padding: 12px;
     border-radius: 4px;
